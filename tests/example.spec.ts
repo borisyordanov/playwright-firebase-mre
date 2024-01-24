f16ffa182e0d8c0a9441a1f5d2a1fc6995947251
Boris <b.yordanov@toki.bg>
2024-01-24 09:23:19 +0200

    make tests runnable

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,25 +1,20 @@
-import {  expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-import { test } from '../auth.setup' //
-
-test('has title', async ({ page, auth }) => {
-
-  await page.goto('/', { waitUntil: 'networkidle' })
-  await auth.login(page) // <-- we need to pass in the pag
-  
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page, auth }) => {
-  await page.goto('https://playwright.dev/');
-  await auth.login(page) // <-- we need to pass in the pag
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
 });
